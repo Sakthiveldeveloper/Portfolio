f618ab2d5fe1b17fcaed2af117927bb4c8dfdc99
Sakthiveldeveloper <sakthivel24498@gmail.com>
2022-05-06 13:34:05 +0530

    add work examples on demo and repo in wrok section

--- src/data/WorkData.js
+++ src/data/WorkData.js
@@ -1,23 +1,42 @@
 // work data
 
 export const Work = [
+    // {
+    //     id:1,
+    //     name:"Agency Landing Page",
+    //     description:"It is build on top of the React JS, with styledComponents and GSAP for smooth scrolling animations.",
+    //     tags:["react","gsap","styledComponents"],
+        
+    //     demo:"https://agency-website-eta.vercel.app/",
+    //     github:"https://github.com/codebucks27/Agency-website"
+    // },
     {
         id:1,
-        name:"Agency Landing Page",
-        description:"It is build on top of the React JS, with styledComponents and GSAP for smooth scrolling animations.",
-        tags:["react","gsap","styledComponents"],
+        name:"Food Delivery Application",
+        description:"A Food Ordering Web site .It is build on top of the React JS, with styledComponents and Bootstrap for smooth scrolling animations.",
+        tags:["react","Bootstrap","styledComponents"],
         
-        demo:"https://agency-website-eta.vercel.app/",
-        github:"https://github.com/codebucks27/Agency-website"
+        demo:"https://zomatodeploye.herokuapp.com/",
+        github:"https://github.com/Sakthiveldeveloper/finalZOMATO"
     },
+    // {
+    //     id:2,
+    //     name:"Job listing website",
+    //     description:"A Job listing website build in ReactJS. With searching and filtering functionalities.",
+    //     tags:["react","sass"],
+        
+    //     demo:"http://react-job-listing-website.codebucks27.vercel.app/",
+    //     github:"https://github.com/codebucks27/react-job-listing-website"
+        
+    // },
     {
         id:2,
-        name:"Job listing website",
-        description:"A Job listing website build in ReactJS. With searching and filtering functionalities.",
+        name:"Univercity listing website",
+        description:"A University listing website build in ReactJS. With searching and filtering functionalities.",
         tags:["react","sass"],
         
-        demo:"http://react-job-listing-website.codebucks27.vercel.app/",
-        github:"https://github.com/codebucks27/react-job-listing-website"
+        demo:"https://uoideploy.herokuapp.com/",
+        github:"https://github.com/Sakthiveldeveloper/lastUOI"
         
     },
     {
