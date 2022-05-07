6984d51037e556871dac37d582d3e510a55c668f
Sakthiveldeveloper <sakthivel24498@gmail.com>
2022-05-07 07:59:32 +0530

    change frontend to fullstackdeveloper

--- src/components/MySkillsPage.js
+++ src/components/MySkillsPage.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import styled, { ThemeProvider } from 'styled-components'
-import {lightTheme} from './Themes';
-import { Design, Develope} from './AllSvgs';
+import { lightTheme } from './Themes';
+import { Design, Develope } from './AllSvgs';
 
 
 import LogoComponent from '../subComponents/LogoComponent';
@@ -85,69 +85,69 @@ ul,p{
 const MySkillsPage = () => {
     return (
         <ThemeProvider theme={lightTheme}>
-<Box>
+            <Box>
+                <LogoComponent theme='light' />
+                <SocialIcons theme='light' />
+                <PowerButton />
+                <ParticleComponent theme='light' />
+                <Main>
+                    <Title>
+                        <Design width={40} height={40} /> Designer
+                    </Title>
+                    <Description>
+                        I love to create design which speaks, Keep it clean, minimal and simple.
+                    </Description>
+                    <Description>
+                        <strong>I like to Design</strong>
+                        <ul>
+                            <li>
+                                Web Design
+                            </li>
+                            <li>
+                                Mobile Apps
+                            </li>
+                        </ul>
+                    </Description>
+                    <Description>
+                        <strong>Tools</strong>
+                        <ul>
+                            <li>
+                                Figma
+                            </li>
 
-<LogoComponent theme='light'/>
-<SocialIcons theme='light'/>
-<PowerButton />
-<ParticleComponent theme='light' />
-            <Main>
-<Title>
-    <Design width={40} height={40} /> Designer
-</Title>
-<Description>
-I love to create design which speaks, Keep it clean, minimal and simple.
-</Description>
-<Description>
-<strong>I like to Design</strong>
-<ul>
-    <li>
-        Web Design
-    </li>
-    <li>
-        Mobile Apps
-    </li>
-</ul>
-</Description>
-<Description>
-<strong>Tools</strong>
-<ul>
-    <li>
-       Figma
-    </li>
-    
-</ul>
-</Description>
+                        </ul>
+                    </Description>
 
-            </Main>
-            <Main>
-<Title>
-    <Develope width={40} height={40} /> Frontend Developer
-</Title>
-<Description>
-I value business or brand for which i'm creating, thus i enjoy bringing new ideas to life.
-</Description>
-<Description>
-<strong>Skills</strong>
-<p>
-Html, Css, Js, React, Redux, Sass, Bootstrap, Tailwind, Firebase etc.
-</p>
-</Description>
-<Description>
-<strong>Tools</strong>
-<p>
-VScode, Github, Codepen etc.
-</p>
-</Description>
+                </Main>
+                <Main>
+                    <Title>
+                        <Develope width={40} height={40} /> FullStack Developer
+                        {/* <Develope width={40} height={40} /> Frontend Developer */}
+                    </Title>
+                    <Description>
+                        I value business or brand for which i'm creating, thus i enjoy bringing new ideas to life.
+                    </Description>
+                    <Description>
+                        <strong>Skills</strong>
+                        <p>
+                            Html, Css, Js, React, Redux, Sass, Bootstrap, Tailwind, Firebase, MaterialUI etc.
+                        </p>
+                    </Description>
+                    <Description>
+                        <strong>Tools</strong>
+                        <p>
+                            VScode, Github, Codepen etc.
+                        </p>
+                    </Description>
 
-            </Main>
+                </Main>
 
-            <BigTitle text="SKILLS" top="80%" right="30%" />
+                <BigTitle text="SKILLS" top="80%" right="30%" />
 
-        </Box>
+            </Box>
 
         </ThemeProvider>
-        
+
     )
 }
 
