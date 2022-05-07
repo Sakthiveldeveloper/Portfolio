113bf086bfe7ab2d902cf1ef8957234f89e76f3d
Sakthiveldeveloper <sakthivel24498@gmail.com>
2022-05-07 07:40:07 +0530

    erase another works

--- src/data/WorkData.js
+++ src/data/WorkData.js
@@ -39,44 +39,44 @@ export const Work = [
         github:"https://github.com/Sakthiveldeveloper/lastUOI"
         
     },
-    {
-        id:3,
-        name:"Dev.to Clone",
-        description:"A clone of Dev.to build using ReactJS. With realtime fetching of articles from dev.to using API.",
-        tags:["react","api","sass"],
+    // {
+    //     id:3,
+    //     name:"Dev.to Clone",
+    //     description:"A clone of Dev.to build using ReactJS. With realtime fetching of articles from dev.to using API.",
+    //     tags:["react","api","sass"],
         
-        demo:"http://devto-clone-codebucks27.vercel.app/",
-        github:"https://github.com/codebucks27/devto-clone"
-    },
-    {
-        id:4,
-        name:"Sidebar Menu",
-        description:"A cool looking sidebar menu build using ReactJS and styledComponents.It has really smooth animations.",
-        tags:["react","styledComponents","react-router"],
+    //     demo:"http://devto-clone-codebucks27.vercel.app/",
+    //     github:"https://github.com/codebucks27/devto-clone"
+    // },
+    // {
+    //     id:4,
+    //     name:"Sidebar Menu",
+    //     description:"A cool looking sidebar menu build using ReactJS and styledComponents.It has really smooth animations.",
+    //     tags:["react","styledComponents","react-router"],
         
-        demo:"https://react-sidebar.vercel.app/",
-        github:"https://github.com/codebucks27/react-sidebar"
+    //     demo:"https://react-sidebar.vercel.app/",
+    //     github:"https://github.com/codebucks27/react-sidebar"
        
-    },{
-        id:5,
-        name:"Todo App(React + Redux)",
-        description:"A todo app build using Redux-toolkit and framer-motion for smooth animations.",
-        tags:["react","redux","framer-motion"],
+    // },{
+    //     id:5,
+    //     name:"Todo App(React + Redux)",
+    //     description:"A todo app build using Redux-toolkit and framer-motion for smooth animations.",
+    //     tags:["react","redux","framer-motion"],
         
-        demo:"http://react-redux-todo-app-git-main-codebucks27.vercel.app/",
-        github:"https://github.com/codebucks27/React-Redux-Todo-App"
+    //     demo:"http://react-redux-todo-app-git-main-codebucks27.vercel.app/",
+    //     github:"https://github.com/codebucks27/React-Redux-Todo-App"
         
-    },
-    {
-        id:6,
-        name:"Responsive Navigation Menu",
-        description:"A reponsive navbar menu component in ReactJS with react-router functionalities. Built from scratch.",
-        tags:["react","react-router","css"],
+    // },
+    // {
+    //     id:6,
+    //     name:"Responsive Navigation Menu",
+    //     description:"A reponsive navbar menu component in ReactJS with react-router functionalities. Built from scratch.",
+    //     tags:["react","react-router","css"],
         
-        demo:"http://react-responsive-navbar-codebucks27.vercel.app/",
-        github:"https://github.com/codebucks27/React-responsive-navbar"
+    //     demo:"http://react-responsive-navbar-codebucks27.vercel.app/",
+    //     github:"https://github.com/codebucks27/React-responsive-navbar"
         
-    },
+    // },
     
   
 ] 
